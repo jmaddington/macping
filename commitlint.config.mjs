5c0f12dfbf978245fd97cccbf0eb0b2edfe161bc
Stanislas Lange <git@slange.me>
2025-12-16 12:35:22 +0100

    ci: disable subject-case rule in commitlint
    Allow proper nouns like README, API, URL to remain uppercase in commit
    message subjects.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -18,7 +18,7 @@ export default {
         'revert',
       ],
     ],
-    'subject-case': [2, 'always', 'lower-case'],
+    'subject-case': [0],
     'header-max-length': [2, 'always', 72],
   },
 };
